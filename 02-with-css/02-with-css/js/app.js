7ab6c07ac1424730d62b8fe24916249ca44aac38
! Jajou <jahelgithub64@gmail.com>
2025-02-01 17:24:55 +0100

    Finish

--- 02-with-css/02-with-css/js/app.js
+++ 02-with-css/02-with-css/js/app.js
@@ -1,52 +1,46 @@
 const $inputRange = document.querySelector("#length");
 const $selectedValue = document.querySelector(".selected-length");
-
-console.log($inputRange);
-console.log($selectedValue);
-
-// Generation du mot de passe
-// 1. 10 lettres en majuscule aléatoire
-// 2. nb (10, 2, 4, 20) dynamique de lettres en majuscule alétatoire
-
-const $uppercaseLetters = document.querySelector("#uppercase");
 const $generatedPassword = document.querySelector(".generated-password");
+const $uppercaseCheckbox = document.querySelector("#uppercase");
+const $lowercaseCheckbox = document.querySelector("#lowercase");
+const $numbersCheckbox = document.querySelector("#numbers");
+const $symbolsCheckbox = document.querySelector("#symbols");
+const $generateButton = document.querySelector("button[type='submit']");
 
-console.log($uppercaseLetters);
-
-function generatePassword(length) {
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+function generatePassword(length, useUppercase, useLowercase, useNumbers, useSymbols) {
+  const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
   const symbols = "&~=#{}([-|_^@])^$*%£%!§:/;.?<>²";
   const numbers = "1234567890";
-  let result = "";
+  let character = "";
+  let password = "";
+
+  if (useUppercase) character += uppercaseLetters;
+  if (useLowercase) character += lowercaseLetters;
+  if (useNumbers) character += numbers;
+  if (useSymbols) character += symbols;
 
   for (let i = 0; i < length; i++) {
-    const randomPassword = Math.floor(Math.random() * 3);
-
-    let newPassword = 0;
-
-    if (randomPassword === 0) {
-      const randomLettersNumber = Math.floor(Math.random() * letters.length);
-      newPassword = letters[randomLettersNumber];
-    } else if (randomPassword === 1) {
-      const randomSymbolsNumber = Math.floor(Math.random() * symbols.length);
-      newPassword = symbols[randomSymbolsNumber];
-    } else {
-      const randomIndexNumbers = Math.floor(Math.random() * numbers.length);
-      newPassword = numbers[randomIndexNumbers];
-    }
-
-    result += newPassword
+    const randomIndex = Math.floor(Math.random() * character.length);
+    password += character[randomIndex];
   }
 
-  console.log(result);
-  console.log(result.length);
+  return password;
 }
-
-console.log(generatePassword());
 
 $inputRange.addEventListener("input", (event) => {
   let valueChanged = event.target.value;
-  console.log(valueChanged);
-
   $selectedValue.textContent = valueChanged;
+});
+
+$generateButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const length = parseInt($inputRange.value);
+  const useUppercase = $uppercaseCheckbox.checked;
+  const useLowercase = $lowercaseCheckbox.checked;
+  const useNumbers = $numbersCheckbox.checked;
+  const useSymbols = $symbolsCheckbox.checked;
+
+  const newPassword = generatePassword(length, useUppercase, useLowercase, useNumbers, useSymbols);
+  $generatedPassword.textContent = newPassword;
 });
